c97df0e5ad52cc43aadab17f2cb0f66b33129729
Adam Zekiec <adam.zekiec@gmail.com>
2025-03-17 16:54:18 +0100

    Added Task 2 functionality

--- basic_reader.py
+++ basic_reader.py
@@ -1,4 +1,6 @@
 import chardet
+import sys
+from collections import deque
 from chardet import UniversalDetector
 
 def detect_encoding(file_path):
@@ -19,4 +21,29 @@ encoding_info = detect_encoding(file_path)
 f = open(file_path, encoding=encoding_info['encoding'], mode = 'rt')
 print(f.read())
 
-# Siema
+with open(file_path, encoding=encoding_info['encoding'], mode = 'rt') as f:
+    sys.stdin = f
+    q=deque()
+    empty_ctr = 0
+    pre_finished = False
+    for line in sys.stdin:
+        clean_line = ' '.join(line.split())
+
+        if len(q) < 10 and not pre_finished:
+            if not clean_line:
+                empty_ctr+=1
+            else:
+                empty_ctr=0
+            if empty_ctr >= 2:
+                q.clear()
+                empty_ctr=0
+                pre_finished=True
+
+        if "-----" in clean_line:
+            while len(q) > 0:
+                print(q.popleft())
+            break
+
+        q.append(clean_line)
+        if len(q) >= 10:
+            print(q.popleft())
